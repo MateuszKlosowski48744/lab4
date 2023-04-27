ed1afbe77e0e1d72841b83909c70decbc3aa0850
Twoja nazwa użytkownika <Twj adres e-mail>
2023-04-27 17:12:46 +0200

    dodalem --logs

--- skrypt.sh
+++ skrypt.sh
@@ -7,6 +7,21 @@ else
   echo "Użycie: $0 --date"
 fi
 
-
+if [ "$1" == "--logs" -o "$1" == "-l" ]; then
+  if [[ $2 =~ ^[0-9]+$ ]]; then
+    num_logs=$2
+  else
+    num_logs=100
+  fi
+  for i in $(seq 1 $num_logs)
+  do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: ${filename}" > ${filename}
+    echo "Nazwa skryptu: skrypt.sh" >> ${filename}
+    echo "Data: $(date)" >> ${filename}
+  done
+else
+  echo "Nieprawidłowa flaga lub brak argumentu liczby plików. Użyj --logs [liczba], aby utworzyć określoną liczbę plików logów lub --logs, aby utworzyć 100 plików."
+fi
 
 
